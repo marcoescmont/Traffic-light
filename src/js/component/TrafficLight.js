ce31a9abb688d29cb34cd0ed28066a943cfdc615
marcoescmont <marcoescmont@gmail.com>
2021-07-08 01:41:59 +0000

    light functionality done, just css left to do

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
+import "../../styles/index.scss";
 
 const TrafficLight = () => {
 	const [currentLight, nextLight] = useState("red");
 
+	let lightCircle = <h1 type="button" className="fas fa-circle"></h1>;
 	return (
-		<div className="container text-center">
-			<h1 className="fas fa-circle text-danger "></h1>
-			<h1 className="fas fa-circle text-warning"></h1>
-			<h1 className="fas fa-circle text-success"></h1>
+		<div className="container text-center trafficLightBody">
+			<div
+				className={
+					currentLight == "red"
+						? "text-danger highlight"
+						: "text-danger"
+				}
+				onClick={() => nextLight("red")}>
+				{lightCircle}
+			</div>
+			<div
+				className={
+					currentLight == "yellow"
+						? "text-warning highlight"
+						: "text-warning"
+				}
+				onClick={() => nextLight("yellow")}>
+				{lightCircle}
+			</div>
+			<div
+				className={
+					currentLight == "green"
+						? "text-success highlight"
+						: "text-success"
+				}
+				onClick={() => nextLight("green")}>
+				{lightCircle}
+			</div>
 		</div>
 	);
 };
