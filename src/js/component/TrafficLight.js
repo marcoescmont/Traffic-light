7810d2615d8926af4e81e61821695330774b4e3c
marcoescmont <marcoescmont@gmail.com>
2021-07-08 02:32:27 +0000

    project completed

--- src/js/component/TrafficLight.js
+++ src/js/component/TrafficLight.js
@@ -2,37 +2,40 @@ import React, { useState } from "react";
 import "../../styles/index.scss";
 
 const TrafficLight = () => {
-	const [currentLight, nextLight] = useState("");
+	const [currentLight, nextLight] = useState("red");
 
 	let lightCircle = <h1 type="button" className="fas fa-circle"></h1>;
 	return (
-		<div className="container text-center trafficLightBody">
-			<div
-				className={
-					currentLight == "red"
-						? "text-danger highlight-red"
-						: "text-danger"
-				}
-				onClick={() => nextLight("red")}>
-				{lightCircle}
-			</div>
-			<div
-				className={
-					currentLight == "yellow"
-						? "text-warning highlight-yellow"
-						: "text-warning"
-				}
-				onClick={() => nextLight("yellow")}>
-				{lightCircle}
-			</div>
-			<div
-				className={
-					currentLight == "green"
-						? "text-success highlight-green"
-						: "text-success"
-				}
-				onClick={() => nextLight("green")}>
-				{lightCircle}
+		<div>
+			<div className="stick"></div>
+			<div className="container text-center trafficLightBody">
+				<div
+					className={
+						currentLight == "red"
+							? "text-danger highlight-red"
+							: "text-danger"
+					}
+					onClick={() => nextLight("red")}>
+					{lightCircle}
+				</div>
+				<div
+					className={
+						currentLight == "yellow"
+							? "text-warning highlight-yellow"
+							: "text-warning"
+					}
+					onClick={() => nextLight("yellow")}>
+					{lightCircle}
+				</div>
+				<div
+					className={
+						currentLight == "green"
+							? "text-success highlight-green"
+							: "text-success"
+					}
+					onClick={() => nextLight("green")}>
+					{lightCircle}
+				</div>
 			</div>
 		</div>
 	);
